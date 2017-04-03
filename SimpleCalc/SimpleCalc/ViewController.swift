96c6b4b950c5f3fb64452ece6395788fd11ce3d2
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-04-03 12:43:11 -0700

    added files

---
+++ SimpleCalc/SimpleCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by Vishesh Sood on 4/3/17.
+//  Copyright © 2017 Vishesh Sood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
